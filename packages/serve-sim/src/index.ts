2f5251d2e89706c853ba872b389bb57cef871b14
Gabe Debes <pacheco@expo.dev>
2026-09-16 22:46:56 -0700

    refactor(preview): remove the metrics cors flag

--- packages/serve-sim/src/index.ts
+++ packages/serve-sim/src/index.ts
@@ -1924,12 +1924,6 @@ program
     (value: string, prev: string[]) => [...prev, value],
     [] as string[],
   )
-  .option(
-    "--metrics-cors-origin <origin>",
-    "Deprecated alias for --cors-origin.",
-    (value: string, prev: string[]) => [...prev, value],
-    [] as string[],
-  )
   .option("-l, --list [device]", "List running streams")
   .option("-k, --kill [device]", "Kill running stream(s)")
   .addHelpText(
@@ -2055,7 +2049,7 @@ Examples:
     } else {
       await serve(startPort ?? 3200, devices, startPort !== undefined, opts.host, {
         stream,
-        corsOrigins: [...opts.corsOrigin, ...opts.metricsCorsOrigin],
+        corsOrigins: opts.corsOrigin,
         frameAncestors: opts.frameAncestor,
         shareUrl: opts.shareUrl,
         debugStreamPath,
